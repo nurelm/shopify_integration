cd487a1cc28fa54bf38c292cd6d5fddbe8b23ee5
Sam Shaaban <sam@nurelm.com>
2014-08-26 08:55:36 -0400

    Fix problem with empty customer addresses

--- lib/address.rb
+++ lib/address.rb
@@ -1,6 +1,8 @@
 class Address
 
   def add_shopify_obj shopify_address
+    return self if shopify_address.nil?
+    
     @address1 = shopify_address['address1']
     @address2 = shopify_address['address2']
     @zipcode = shopify_address['zip']
@@ -13,6 +15,8 @@ class Address
   end
   
   def add_wombat_obj wombat_address
+    return self if wombat_address.nil?
+
     @address1 = wombat_address['address1']
     @address2 = wombat_address['address2']
     @zipcode = wombat_address['zipcode']
@@ -26,25 +30,25 @@ class Address
   
   def wombat_obj
     {
-      'address1' => @address1,
-      'address2' => @address2,
-      'zipcode' => @zipcode,
-      'city' => @city,
-      'state' => @state,
-      'country' => @country,
-      'phone' => @phone
+      'address1' => @address1.nil? ? "" : @address1,
+      'address2' => @address2.nil? ? "" : @address2,
+      'zipcode' => @zipcode.nil? ? "" : @zipcode,
+      'city' => @city.nil? ? "" : @city,
+      'state' => @state.nil? ? "" : @state,
+      'country' => @country.nil? ? "" : @country,
+      'phone' => @phone.nil? ? "" : @phone
     }
   end
   
   def shopify_obj
     {
-      'address1' => @address1,
-      'address2' => @address2,
-      'zip' => @zipcode,
-      'city' => @city,
-      'province' => @state,
-      'country' => @country,
-      'phone' => @phone
+      'address1' => @address1.nil? ? "" : @address1,
+      'address2' => @address2.nil? ? "" : @address2,
+      'zip' => @zipcode.nil? ? "" : @zipcode,
+      'city' => @city.nil? ? "" : @city,
+      'province' => @state.nil? ? "" : @state,
+      'country' => @country.nil? ? "" : @country,
+      'phone' => @phone.nil? ? "" : @phone
     }
   end
 
