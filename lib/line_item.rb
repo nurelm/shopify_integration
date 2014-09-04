914376a5f643198168110fe8d203f79c19cb3a25
Sam Shaaban <sam@nurelm.com>
2014-09-04 19:47:37 -0400

    Use SKU for Wombat order IDs

--- lib/line_item.rb
+++ lib/line_item.rb
@@ -3,6 +3,7 @@ class LineItem
   def add_shopify_obj shopify_li, shopify_api
     @shopify_id = shopify_li['id']
     @shopify_product_id = shopify_li['product_id']
+    @sku = shopify_li['sku'].blank? ? "SKU Must Be Set!" : shopify_li['sku']
     @name = shopify_li['name']
     @quantity = shopify_li['quantity'].to_i
     @price = shopify_li['price']
@@ -13,8 +14,9 @@ class LineItem
   def wombat_obj
     [
       {
-        'id' => @shopify_id,
-        'product_id' => @shopify_product_id,
+        'id' => @sku,
+        'shopify_id' => @shopify_id,
+        'shopify_product_id' => @shopify_product_id,
         'name' => @name,
         'quantity' => @quantity,
         'price' => @price
