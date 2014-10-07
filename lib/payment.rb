ae7289335e60dd4492c306d1480855ef5fffe10e
Bruno Buccolo <bruno.buccolo@gmail.com>
2014-10-07 16:33:50 -0300

    Uses float on payment amount

--- lib/payment.rb
+++ lib/payment.rb
@@ -3,16 +3,16 @@ class Payment
   def add_shopify_obj shopify_transaction, shopify_api
     @amount = shopify_transaction.amount
     @payment_method = shopify_transaction.gateway
-    
+
     self
   end
-  
+
   def wombat_obj
     {
       'status' => 'completed',
-      'amount' => @amount,
+      'amount' => @amount.to_f,
       'payment_method' => @payment_method
-    } 
+    }
   end
 
 end
