fe38ac180ffe52c2ef95e21672f49dc5790aa6a6
Bruno Buccolo <bruno.buccolo@gmail.com>
2014-10-10 17:09:31 -0300

    Searches by order_id if shopify_order_id is not present on *_shipment

--- lib/shopify/shipment.rb
+++ lib/shopify/shipment.rb
@@ -7,11 +7,11 @@ module Shopify
       @config = config
     end
 
-    def update!
-      if @shipment['status'] == "shipped" && @shipment['shopify_order_id']
+    def ship!
+      if @shipment['status'] == "shipped" && shopify_order_id
         begin
           api_post(
-            "orders/#{@shipment['shopify_order_id']}/fulfillments.json",
+            "orders/#{shopify_order_id}/fulfillments.json",
             {
               fulfillment: { tracking_number: @shipment['tracking'] }
             }
@@ -22,6 +22,28 @@ module Shopify
 
         "Updated shipment #{@shipment['id']} with tracking number #{@shipment['tracking']}."
       end
+    end
+
+    def shopify_order_id
+      @shopify_order_id ||= @shipment['shopify_order_id'] || find_order_id_by_order_number(@shipment['order_id'])
+    end
+
+    def find_order_id_by_order_number(order_number)
+      count = (api_get 'orders/count')['count']
+      page_size = 250
+      pages = (count / page_size.to_f).ceil
+      current_page = 1
+
+      while current_page <= pages do
+        response = api_get 'orders',
+                           {'limit' => page_size, 'page' => current_page}
+        current_page += 1
+        response['orders'].each do |order|
+          return order['id'].to_s if order['order_number'].to_s == order_number
+        end
+      end
+
+      return nil
     end
   end
 end
--- shopify_integration.rb
+++ shopify_integration.rb
@@ -4,8 +4,8 @@ require "endpoint_base"
 require_all 'lib'
 
 class ShopifyIntegration < EndpointBase::Sinatra::Base
-  post '/update_shipment' do
-    summary = Shopify::Shipment.new(@payload['shipment'], @config).update!
+  post '/*_shipment' do # /add_shipment or /update_shipment
+    summary = Shopify::Shipment.new(@payload['shipment'], @config).ship!
 
     result 200, summary
   end
